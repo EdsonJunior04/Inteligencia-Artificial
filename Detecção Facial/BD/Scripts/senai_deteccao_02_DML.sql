0a607270bfd2dd646a0b443dde9a703d7426bafc
Edson Junior <ejr_04@outlook.com>
2022-02-01 21:23:34 -0300

    Projeto iniciado

---
+++ Detecção Facial/BD/Scripts/senai_deteccao_02_DML.sql
@@ -0,0 +1,16 @@
+USE deteccao_facial
+GO
+
+--TIPO USUARIO
+INSERT INTO TIPOUSUARIO(tipo)
+VALUES ('Usuario'),('Administrador');
+GO
+
+--USUARIO
+INSERT INTO USUARIO(idTipoUsuario,nome,email,senha)
+VALUES ('2', 'Administrador', 'adm@adm.com', 'adm12345'),
+	   ('1','Edson','edson@gmail.com','edson123')
+GO
+
+select * from USUARIO
+go
